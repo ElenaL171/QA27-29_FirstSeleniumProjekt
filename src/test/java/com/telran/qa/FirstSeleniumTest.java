f16c7693b9d22f4599a3f06f380c8f6bc57e8d8c
Elena Lebedeva <89071540+ElenaL171@users.noreply.github.com>
2022-06-21 12:40:45 +0200

    Added FirstFindElementTests

---
+++ src/test/java/com/telran/qa/FirstSeleniumTest.java
@@ -0,0 +1,49 @@
+package com.telran.qa;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class FirstSeleniumTest {
+
+    WebDriver driver;
+
+    //before - setUp
+    @BeforeMethod
+    public void setUp(){
+        //driver=new ChromeDriver();
+        driver=new FirefoxDriver();
+        //driver.get("https://www.google.com");
+        driver.get("https://www.google.com");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+    //Test
+    @Test
+    public void openGoogleTest(){
+        System.out.println("Site opened!");
+    }
+    @Test
+    public void navgateTest(){
+        //go back
+        driver.navigate().back();
+        //go forward
+        driver.navigate().forward();
+        //refresh the page
+        driver.navigate().refresh();
+    }
+    //after - tearDown
+    @AfterMethod(enabled = false)
+    public void TearDown(){
+        //close browser
+        //all tabs
+        driver.quit();
+        //one tab
+        //driver.close();
+    }
+}
